c44cceb8109c5763f99ab516c32ac8b046197aab
Ubuntu <ubuntu@fb01.decoded.com>
2023-12-11 21:38:21 +0000

    Adding functionality for register nd login

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,14 @@
+//Below function...
+
 async function showOptions(category) {
-    const response = await fetch('http://fb01.decoded.com:5000/api/gigs');
+    const response = await fetch('gigs.json');    //'http://fb01.decoded.com:5000/api/gigs'
     const data = await response.json();
 
     const optionsContainer = document.getElementById('optionsContainer');
     optionsContainer.innerHTML = '';
 
     const uniqueOptions = new Set();
-    data.forEach(gig => uniqueOptions.add(gig[category]));
+    data.gigs.forEach(gig => uniqueOptions.add(gig[category]));
 
     const optionsList = document.createElement('div');
     uniqueOptions.forEach(option => {
@@ -33,6 +35,85 @@ async function showOptions(category) {
     optionsContainer.style.display = 'block';
 }
 
+
+//Below code is for registration
+
+function showRegistrationForm() {
+    const registrationForm = document.getElementById('registrationForm');
+    registrationForm.style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const userRegistrationForm = document.getElementById('userRegistrationForm');
+
+    userRegistrationForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Get user input values
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Store user details in local storage
+        const userDetails = {
+            username: username,
+            email: email,
+            password: password
+        };
+        localStorage.setItem('userDetails', JSON.stringify(userDetails));
+
+        // Hide the registration form after registration
+        const registrationForm = document.getElementById('registrationForm');
+        registrationForm.style.display = 'none';
+    });
+});
+
+//Below code is for login
+
+function handleLogin() {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.style.display = 'block';
+}
+
+function loginUser() {
+    const storedUserDetails = JSON.parse(localStorage.getItem('userDetails'));
+    const loginForm = document.getElementById('loginForm');
+    const enteredUsername = document.getElementById('loginUsername').value;
+    const enteredPassword = document.getElementById('loginPassword').value;
+
+    if (
+        storedUserDetails &&
+        enteredUsername === storedUserDetails.username &&
+        enteredPassword === storedUserDetails.password
+    ) {
+        // Clear login form
+        loginForm.style.display = 'none';
+
+        // Hide the login and register buttons
+        const loginButton = document.getElementById('login');
+        const registerButton = document.getElementById('register');
+        const loginText = document.getElementById('AR');
+        const registerText = document.getElementById('FTH');
+        
+        loginButton.style.display = 'none';
+        registerButton.style.display = 'none';
+        loginText.style.display = 'none';
+        registerText.style.display = 'none';
+
+        // Show user profile section
+        const userProfile = document.createElement('section');
+        userProfile.textContent = `Welcome, ${enteredUsername}! (User Profile)`;
+        userProfile.setAttribute('id', 'userProfile');
+        document.getElementById('homePageContainer').appendChild(userProfile);
+    } else {
+        // Display an error message for incorrect login details
+        alert('Incorrect username or password. Please try again.');
+    }
+}
+
+
+//Below function...
+
 function showGigsPage(category, selectedOption, data) {
     const homePage = document.getElementById('homePage');
     const gigsPage = document.getElementById('gigsPage');
@@ -47,7 +128,7 @@ function showGigsPage(category, selectedOption, data) {
     gigsLogo.classList.add('logo');
     gigsPageContainer.appendChild(gigsLogo);
 
-    const filteredGigs = data.filter(gig => gig[category] === selectedOption);
+    const filteredGigs = data.gigs.filter(gig => gig[category] === selectedOption);
 
     const gigTemplate = `
         <div class="gig-element">
